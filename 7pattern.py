b5eed343a3b42318278dc4c2233c9a3607d73e51
jeevankumarguduru <jeevankumarguduru3@gmail.com>
2026-03-11 23:35:58 +0530

    7pattern.py created and transfer the code from 6 -->7 file

---
+++ 7pattern.py
@@ -0,0 +1,10 @@
+n = 4
+
+for i in range(1,n+1):
+    for j in range(1,(n+1)*2):
+        if (i>=j):
+            print(j,end=" ")
+        elif (i<=j): 
+            print("-",end=" ")
+        elif ()
+    print()
