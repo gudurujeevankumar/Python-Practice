335ba66b723516cabf07ccfae33b46bd98eef106
jeevankumarguduru <jeevankumarguduru3@gmail.com>
2026-03-12 10:50:51 +0530

    7pattern.py

--- 7pattern.py
+++ 7pattern.py
@@ -1,11 +1,17 @@
 n = 4
 
-for i in range(1,n+1):
-    for j in range(1,(n+1)*2):
-        if (i>=j):
-            print(j,end=" ")
-        elif (i<=j): 
-            print("-",end=" ")
-        elif ():
-            p 
+for i in range(1, n+1):
+
+    # increasing numbers
+    for j in range(1, i+1):
+        print(j, end=" ")
+
+    # spaces
+    for j in range(1, 2*(n-i)+1):
+        print(" ", end=" ")
+
+    # decreasing numbers
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+
     print()
