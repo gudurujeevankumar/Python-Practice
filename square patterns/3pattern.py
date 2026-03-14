480176f2221a636fe144a4a10181d5820f7011c2
jeevankumarguduru <jeevankumarguduru3@gmail.com>
2026-03-14 14:56:01 +0530

    commit

--- square patterns/3pattern.py
+++ square patterns/3pattern.py
@@ -2,7 +2,7 @@ n = 4
 count = 0
 for i in range(n):
     for j in range(1,n+1):
-        count = count + 1
+        count += 1  # type: ignore
         print(count, end=" ")
         if count==9 : count = 0
     print()
