9a2d4a027dd1e941696dff41ce485a7fbdb33d5b
jeevankumarguduru <jeevankumarguduru3@gmail.com>
2026-03-18 21:37:21 +0530

    2.⁠ ⁠Check whether a string is a palindrome

---
+++ class tasks/18th march/Mock Practice/stringpal.py
@@ -0,0 +1,19 @@
+s = input("Enter the string : ")
+revS = ""
+count = 0
+
+for char in s:
+    count += 1
+
+
+for i in range (count,0,-1):
+    revS += s[i-1]
+
+if s == revS:
+    print("Given string : " + s)
+    print("Reversed string : " + revS)
+    print("So, Given string is a palindrom")
+else:
+    print("Given string : " + s)
+    print("Reversed string : " + revS)
+    print("So, Given string is not a palindrom")
